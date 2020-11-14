5e1358aebee1719b1aa4a74de5641e7276abe5a6
rafael.medeiros.am@gmail.com <rafael.medeiros.am@gmail.com>
2020-11-14 15:44:34 -0400

    correcoes de CategoriaService e contrutor de Pagamento

--- src/main/java/com/rafael/medeiros/cursomsc/services/CategoriaService.java
+++ src/main/java/com/rafael/medeiros/cursomsc/services/CategoriaService.java
@@ -33,9 +33,14 @@ public class CategoriaService {
 	}
 	
 	public Categoria update(Categoria obj) {
-		find(obj.getId());
-		return repo.save(obj);
+		Categoria newObj = find(obj.getId());
+		updateData(newObj,obj);
+		return repo.save(newObj);
 	}
+	
+	
+	
+	
 	
 	public void delete (Integer id) {
 		find(id);
@@ -56,6 +61,13 @@ public class CategoriaService {
 	public Categoria fromDto(CategoriaDTO objDto) {
 		return new Categoria(objDto.getId(), objDto.getNome());
 	}
+	
+	private void updateData(Categoria newObj,Categoria obj) {
+		newObj.setNome(obj.getNome());
+				
+	}
+
+	
 	
 }
 	
