7a970779d79a913a0b4c74cc085090890abfa3c5
rafael.medeiros.am@gmail.com <rafael.medeiros.am@gmail.com>
2020-11-13 23:03:02 -0400

    Paginação com parametros opcionais

--- src/main/java/com/rafael/medeiros/cursomsc/services/CategoriaService.java
+++ src/main/java/com/rafael/medeiros/cursomsc/services/CategoriaService.java
@@ -5,6 +5,10 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.querydsl.QPageRequest;
 import org.springframework.stereotype.Service;
 
 import com.rafael.medeiros.cursomsc.domain.Categoria;
@@ -44,7 +48,11 @@ public class CategoriaService {
 	public List<Categoria> findAll(){
 		return repo.findAll();
 	}
-	
+	public Page<Categoria> findPage(Integer page,Integer linesPerPage, String orderBy, String direction){
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		return repo.findAll(pageRequest);
+		
+	}
 }
 	
 
