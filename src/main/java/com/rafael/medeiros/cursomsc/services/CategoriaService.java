2ebe8341740a2ab22cab73dd399c5ec71806186f
rafael.medeiros.am@gmail.com <rafael.medeiros.am@gmail.com>
2020-11-13 21:13:05 -0400

    Deletando uma categoria com o delete, com tratamento de exception

--- src/main/java/com/rafael/medeiros/cursomsc/services/CategoriaService.java
+++ src/main/java/com/rafael/medeiros/cursomsc/services/CategoriaService.java
@@ -3,10 +3,12 @@ package com.rafael.medeiros.cursomsc.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.rafael.medeiros.cursomsc.domain.Categoria;
 import com.rafael.medeiros.cursomsc.repositories.CategoriaRepository;
+import com.rafael.medeiros.cursomsc.services.exceptions.DataIntegrityException;
 import com.rafael.medeiros.cursomsc.services.exceptions.ObjectNotFoundException;
 
 @Service
@@ -29,6 +31,17 @@ public class CategoriaService {
 		find(obj.getId());
 		return repo.save(obj);
 	}
+	
+	public void delete (Integer id) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		} catch(DataIntegrityViolationException e){
+			throw new DataIntegrityException("Não e possível excluir uma categoria que possue produtos!");
+		}
+	}
+	
+	
 }
 	
 
