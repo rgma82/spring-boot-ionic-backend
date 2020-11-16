4e9cf0c827d0ee9310774dcb636337da8fc71204
rafael.medeiros.am@gmail.com <rafael.medeiros.am@gmail.com>
2020-11-15 23:13:02 -0400

    Busca de produtos por nome e categoria

--- src/main/java/com/rafael/medeiros/cursomsc/repositories/ProdutoRepository.java
+++ src/main/java/com/rafael/medeiros/cursomsc/repositories/ProdutoRepository.java
@@ -1,11 +1,20 @@
 package com.rafael.medeiros.cursomsc.repositories;
 
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.rafael.medeiros.cursomsc.domain.Categoria;
 import com.rafael.medeiros.cursomsc.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>{
-
+	@Transactional(readOnly = true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(String nome, List<Categoria> categorias,Pageable pageRequest);
+	
 }
